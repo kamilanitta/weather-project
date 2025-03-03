2a55fc9108d1531360d7f5cea5843d8d9e4cb761
Kamila Nitta <kamila.nitta@gmail.com>
2025-03-03 21:51:43 +0100

    css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
+import { Weather } from "./Weather/Weather"
 import "./App.css"
 
 function App() {
   return (
-    <div>
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/kamilanitta/weather-project"
-          target="_blank"
-        >
-          Kamila Nitta
-        </a>
-      </footer>
+    <div className="app">
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/kamilanitta/weather-project"
+            target="_blank"
+          >
+            Kamila Nitta
+          </a>
+        </footer>
+      </div>
     </div>
   )
 }
